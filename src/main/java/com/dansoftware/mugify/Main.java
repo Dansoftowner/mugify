4ac53faa0709858263a73463adc24f89cb24b843
Daniel Gyoerffy <judalion777@gmail.com>
2025-03-27 20:52:26 +0100

    :sparkles: Mug generation file menu item

--- src/main/java/com/dansoftware/mugify/Main.java
+++ src/main/java/com/dansoftware/mugify/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
                 });
             }
 
-        }).start();
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/com/dansoftware/mugify/gui/MugifyMenuBar.java
+++ src/main/java/com/dansoftware/mugify/gui/MugifyMenuBar.java
@@ -1,18 +1,36 @@
 package com.dansoftware.mugify.gui;
 
+import com.dansoftware.mugify.mug.MugRandomizer;
 import javafx.scene.control.*;
 
 public class MugifyMenuBar extends MenuBar {
     private final MugGrid mugGrid;
+    private final MugRandomizer randomizer;
 
     public MugifyMenuBar(MugGrid mugGrid) {
         this.mugGrid = mugGrid;
+        this.randomizer = new MugRandomizer();
         this.buildMenuStructure();
     }
 
     private void buildMenuStructure() {
-        this.getMenus().add(new Menu("File"));
+        this.getMenus().add(buildFileMenu());
         this.getMenus().add(buildViewMenu());
+    }
+
+    private Menu buildFileMenu() {
+        var menu = new Menu("File");
+
+        var fileOpenItem = new MenuItem("Open file");
+        menu.getItems().add(fileOpenItem);
+
+        var fileSaveItem = new MenuItem("Save file");
+        menu.getItems().add(fileSaveItem);
+
+        var generateItem = new MenuItem("Generate new mug");
+        generateItem.setOnAction(_ -> randomizer.apply(mugGrid.getMugTuple()));
+        menu.getItems().add(generateItem);
+        return menu;
     }
 
     private Menu buildViewMenu() {
