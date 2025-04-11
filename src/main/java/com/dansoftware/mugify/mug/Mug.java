3b2123546d38a361b00ba47aaefeebc21d465e70
Daniel Gyoerffy <judalion777@gmail.com>
2025-04-11 20:27:47 +0200

    :zap: Use smaller number of segments in handle

--- src/main/java/com/dansoftware/mugify/mug/Mug.java
+++ src/main/java/com/dansoftware/mugify/mug/Mug.java
@@ -131,7 +131,7 @@ public class Mug extends Group implements MugLike {
         var handleMaterial = new PhongMaterial();
         handleMaterial.diffuseColorProperty().bind(this.handleColor);
 
-        int numSegments = 1000;
+        int numSegments = 50;
         double arcAngle = 180.0;
 
         for (int i = 0; i < numSegments; i++) {
