6e64d9e1829e857a0313794dc5480b7618ab125a
Daniel Gyoerffy <judalion777@gmail.com>
2025-04-13 16:20:49 +0200

    :adhesive_bandage: Adjusted area & surface calculations

--- src/main/java/com/dansoftware/mugify/mug/MugSurfaceVolumeCalculator.java
+++ src/main/java/com/dansoftware/mugify/mug/MugSurfaceVolumeCalculator.java
@@ -1,5 +1,7 @@
 package com.dansoftware.mugify.mug;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.value.ObservableValue;
 
@@ -8,16 +10,19 @@ public class MugSurfaceVolumeCalculator {
     }
 
     public static ObservableValue<? extends Number> getVolumeProperty(MugLike mug) {
+        return bodyVolume(mug).add(handleVolume(mug));
+    }
+
+    public static ObservableValue<? extends Number> getSurfaceAreaProperty(MugLike mug) {
+        return bodySurfaceArea(mug).add(handleSurfaceArea(mug)).subtract(handleBaseArea(mug).multiply(2));
+    }
+
+    private static DoubleBinding bodyVolume(MugLike mug) {
         /*
-           Calculation:
-              1) Mug body:
+         Calculation:
+                Mug body:
                  outer_cylinder_volume = radius^2 * PI * height
                  inner_cylinder_volume =  (radius - border_thickness * 2)^2 * PI * height
-
-                 outer_cylinder_volume - inner_cylinder_volume
-              2) Handle:
-                 outer_cylinder_volume / 2 = handle_radius^2 * PI * height / 2
-                 inner_cylinder_volume / 2 = (handle_radius - handle_width * 2)^2 * PI * height / 2
 
                  outer_cylinder_volume - inner_cylinder_volume
          */
@@ -25,8 +30,6 @@ public class MugSurfaceVolumeCalculator {
         DoubleProperty radius = mug.radiusProperty();
         DoubleProperty borderThickness = mug.borderThicknessProperty();
         DoubleProperty height = mug.heightProperty();
-        DoubleProperty handleRadius = mug.handleRadiusProperty();
-        DoubleProperty handleWidth = mug.handleWidthProperty();
 
         var radiusPow = radius.multiply(radius); // radius^2
         var outerBodyVolume = radiusPow.multiply(Math.PI).multiply(height); // radius^2*PI*height
@@ -34,26 +37,27 @@ public class MugSurfaceVolumeCalculator {
         var innerRadiusPow = innerRadius.multiply(innerRadius);
         var innerBodyVolume = innerRadiusPow.multiply(Math.PI).multiply(height);
 
-        var mugBodyVolume = outerBodyVolume.subtract(innerBodyVolume);
-
-        var handleRadiusPow = handleRadius.multiply(handleRadius);
-        var outerHandleVolume = handleRadiusPow.multiply(Math.PI).multiply(handleWidth).divide(2);
-        var innerHandleRadius = handleRadius.subtract(handleWidth.multiply(2));
-        var innerHandleRadiusPow = innerHandleRadius.multiply(innerHandleRadius);
-        var innerHandleVolume = innerHandleRadiusPow.multiply(Math.PI).multiply(handleWidth).divide(2);
-
-        var handleVolume = outerHandleVolume.subtract(innerHandleVolume);
-
-        return mugBodyVolume.add(handleVolume);
+        return outerBodyVolume.subtract(innerBodyVolume);
     }
 
-    public static ObservableValue<? extends Number> getSurfaceAreaProperty(MugLike mug) {
+    private static DoubleBinding handleVolume(MugLike mug) {
+        /*
+         Calculation:
+               perimeter = handleRadius * PI
+               base = { if handle is rounded: (handleWidth/2)^2 * PI, else: handleWidth^2 }
+               handle_volume = base * perimeter
+         */
 
+        var baseArea = handleBaseArea(mug);
+        var arcLength = mug.handleRadiusProperty().multiply(Math.PI);
+
+        return baseArea.multiply(arcLength);
+    }
+
+    private static DoubleBinding bodySurfaceArea(MugLike mug) {
         DoubleProperty radius = mug.radiusProperty();
         DoubleProperty borderThickness = mug.borderThicknessProperty();
         DoubleProperty height = mug.heightProperty();
-        DoubleProperty handleRadius = mug.handleRadiusProperty();
-        DoubleProperty handleWidth = mug.handleWidthProperty();
 
         var innerRadius = radius.subtract(borderThickness.multiply(2));
 
@@ -63,8 +67,31 @@ public class MugSurfaceVolumeCalculator {
         var outerBodySurface = radius.multiply(2 * Math.PI).multiply(height);
         var innerBodySurface = innerRadius.multiply(2 * Math.PI).multiply(height);
 
-        // TODO: handle surface
+        var borderSurface = bottomSurface.subtract(innerBottomSurface);
 
-        return outerBodySurface.add(innerBodySurface);
+        return outerBodySurface.add(innerBodySurface).add(bottomSurface).add(innerBottomSurface).add(borderSurface);
+    }
+
+    private static DoubleBinding handleSurfaceArea(MugLike mug) {
+        var arcLength = mug.handleRadiusProperty().multiply(Math.PI);
+        var basePerimeter = Bindings.createDoubleBinding(() -> {
+            if (mug.isHandleRounded()) {
+                double r = mug.getHandleWidth() / 2;
+                return 2 * r * Math.PI;
+            }
+            return 4 * mug.getHandleWidth();
+        }, mug.handleRoundedProperty(), mug.handleWidthProperty());
+
+        return basePerimeter.multiply(arcLength);
+    }
+
+    private static DoubleBinding handleBaseArea(MugLike mug) {
+        return Bindings.createDoubleBinding(() -> {
+            if (mug.isHandleRounded()) {
+                double r = mug.getHandleWidth() / 2;
+                return Math.pow(r, 2) * Math.PI;
+            }
+            return Math.pow(mug.getHandleWidth(), 2);
+        }, mug.handleRoundedProperty(), mug.handleWidthProperty());
     }
 }
